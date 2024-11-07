5e10f74e05c642afd3ce6c836764cec8345913a5
antonia trombetta <antonia.dorazio@gmail.com>
2024-11-07 11:58:11 +0100

    fixed styles for ToDo project

--- src/Pages/Filter.jsx
+++ src/Pages/Filter.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import styles from './ToDo.module.css'
+import styles from './ToDo.module.css';
 
-const Filter = ({filter, setFilter, setSort}) => {
+const Filter = ({ filter, setFilter, setSort }) => {
   return (
-    <div className="filter">
+    <div className={styles.filter}>
       <h2>Filter</h2>
-      <div className="filter-options">
-        <p>status:</p>
-        <select value={filter} onChange={(e)=> setFilter(e.target.value)} >
+      <div className={styles.filterOptions}>
+        <div className={styles.statusContainer}>
+          <p className={styles.statusLabel}>Status:</p>
+          <select
+            className={styles.statusSelect}
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
             <option value="All">All</option>
             <option value="Completed">Completed</option>
             <option value="Incomplete">Incomplete</option>
-        </select>
-      </div>
-      <div>
-        <p>A to Z:</p>
-        <button onClick={()=> setSort("Asc")}>Asc</button>
-        <button onClick={()=> setSort("Desc")}>Desc</button>
+          </select>
+        </div>
+        <div className={styles.sortContainer}>
+          <p>A to Z:</p>
+          <button onClick={() => setSort("Asc")}>Asc</button>
+          <button onClick={() => setSort("Desc")}>Desc</button>
+        </div>
       </div>
     </div>
   );
